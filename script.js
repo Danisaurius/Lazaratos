24f91922c1e2cab0dfc33579eb5e28088198a104
Danisaurius <dagores@gmail.com>
2025-08-03 22:01:20 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+  setTimeout(function () {
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+    document.body.classList.add('loaded');
+  }, 5000); // 2000ms = 2s
+});
