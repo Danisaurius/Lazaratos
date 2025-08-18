ebdc91d22a1cd59913c673a382618352eedd1e3c
Danisaurius <dagores@gmail.com>
2025-08-18 00:03:01 -0500

    add nav links

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ window.addEventListener('load', function () {
     document.getElementById('preloader').style.display = 'none';
     document.getElementById('main-content').style.display = 'block';
     document.body.classList.add('loaded');
-  }, 3000); // 2000ms = 2s
+  }, 2000); // 2000ms = 2s
 });
